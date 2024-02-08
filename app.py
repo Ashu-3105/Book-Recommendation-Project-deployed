b52d3e1c0089d5c8e6b01a1b86b4603be519ddbe
Ashutosh Kumar Pathak <83453301+Ashu-3105@users.noreply.github.com>
2024-02-08 13:05:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,10 @@ def recommend():
 
     return render_template('recommend.html',data=data)
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
